11edcefb3f0bfa13223aca7173d2c7d30f066fdd
Subind <subind214@gmail.com>
2022-07-12 12:54:55 +0530

    Basic textView binding

--- app/src/main/java/com/example/databindingpractice/MainActivity.kt
+++ app/src/main/java/com/example/databindingpractice/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.databindingpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.databindingpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /*setContentView(R.layout.activity_main)
+        val myTextView = findViewById<TextView>(R.id.hello_tv)
+        myTextView.text = getString(R.string.hello_world)*/
+
+        /**
+         * Step 3: Wrap the current layout within a layout tag, the dataBinding library generates the binding class 'ActivityMainBinding'
+         * DataBindingUtil's setContentView is used to inflate the layout
+         * Using the instance of which the id's within the layout can be accessed.
+         */
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.helloTv.text = getString(R.string.hello_world)
     }
 }
